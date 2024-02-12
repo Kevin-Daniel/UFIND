f3e25e51ce02994a039b47b4f38aa5134fb41a81
Kevin Daniel <kevinadaniel7@gmail.com>
2024-02-12 16:23:12 -0500

    forced graph, nodes with names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,34 @@
-import React, {useState, useRef, useEffect } from 'react';
-import * as d3 from 'd3';
-import './App.css';
+import "./styles.css";
+import styled from "styled-components";
+import { CssBaseline } from "@mui/material";
+import ForceGraph from "./ForceGraph";
 
-function App() {
-const [data] = useState([25, 50, 35, 15, 94, 10]);
-const svgRef = useRef();
+const Main = styled("main")`
+  display: flex;
+  height: 100vh;
+`;
 
-useEffect(() => {
-  // setting up svg
-  const w = 400;
-  const h = 100;
-  const svg = d3.select(svgRef.current)
-    .attr('width', w)
-    .attr('height', h)
-    .style('background', '#d3d3d3')
-    .style('margin-top', '50')
-    .style('overflow', 'visible');
-
-  // setting the scaling
-  const xScale = d3.scaleLinear()
-    .domain([0, data.length - 1])
-    .range([0, w]);
-  const yScale = d3.scaleLinear()
-    .domain([0, h])
-    .range([h, 0]);
-  const generateScaledLine = d3.line()
-    .x((d, i) => xScale(i))
-    .y(yScale)
-    .curve(d3.curveCardinal);
-
-  // setting the axes
-  const xAxis = d3.axisBottom(xScale)
-    .ticks(data.length)
-    .tickFormat(i => i + 1);
-  const yAxis = d3.axisLeft(yScale)
-    .ticks(5);
-  svg.append('g')
-    .call(xAxis)
-    .attr('transform', `translate(0, ${h})`);
-  svg.append('g')
-    .call(yAxis);
-
-  // setting up the data for the svg
-  svg.selectAll('.line')
-    .data([data])
-    .join('path')
-      .attr('d', d=> generateScaledLine(d))
-      .attr('fill', 'none')
-      .attr('stroke', 'black');
-}, [data]);
-
+export default function App() {
   return (
-    <div className="App">
-      <svg ref={svgRef}></svg>
-    </div>
+    <Main>
+      <CssBaseline />
+      {/*       <Sidebar>
+        <ToolBox />
+        <EditorBox
+          defaultValue={JSON.stringify(miserables.nodes, null, 2)}
+          title="Nodes"
+        />
+        <EditorBox
+          defaultValue={JSON.stringify(miserables.links, null, 2)}
+          title="Links"
+        />
+      </Sidebar> */}
+
+      <ForceGraph />
+
+      {/*       <GraphItem>
+        <ForceGraph />
+      </GraphItem> */}
+    </Main>
   );
 }
-
-export default App;
