1f08bc153829cceb886474242f9a7291e61a35fc
Kevin-Daniel <kevinadaniel7@gmail.com>
2024-04-02 22:06:47 -0400

    react auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,14 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import { ProgramContextProvider } from './context/ProgramContext'
+import { AuthContextProvider } from './context/AuthContext'
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-    <ProgramContextProvider>
-        <App />
-    </ProgramContextProvider>
+    <AuthContextProvider>
+        <ProgramContextProvider>
+            <App />
+        </ProgramContextProvider>
+    </AuthContextProvider>
 );
