602c114d41bd364f6d43cbf9c123243663fd09e0
Kevin-Daniel <kevinadaniel7@gmail.com>
2024-04-15 10:39:23 -0400

    initial authenetication, working signup and login

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import '../output.css';
+import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const { logout } = useLogout();
+    const { user } = useAuthContext();
+
+    const handleClick = () => {
+        logout();
+    }
+
     return (
         <header className="bg-[#285797] w-full h-[10vh] absolute top-0 flex items-center justify-center text-white" >
             <p className="text-4xl">UFIND</p>
+            {user && (
+                <div>
+                    <span>{user.email}</span>
+                    <button onClick={handleClick}>Log out</button>
+                </div>
+            )}
+            {!user && (
+                <div>
+                    <Link to="/login">Login</Link>
+                    <Link to="/signup">Signup</Link>
+                </div>
+            )}
         </header>
     );
 };
